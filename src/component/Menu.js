a3ee3f11679d09f9254cceec068488c230830b09
Surajit Sahoo <surajitsahoo200@gmail.com>
2025-09-11 00:02:41 +0530

    Changed

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -9,7 +9,7 @@ const Menu=()=>
             <Link className="list-group-item list-group-item-action " tag="a"  to="/" action>Home</Link>
             <Link className="list-group-item list-group-item-action " tag="a"  to="/Add-Courses" action>Add Courses</Link>
             <Link className="list-group-item list-group-item-action "  tag="a"  to="/View-Courses" action>View Courses</Link>
-            <Link className="list-group-item list-group-item-action " tag="a" to="/AbouttUs" action>About Us</Link>
+            <Link className="list-group-item list-group-item-action " tag="ab" to="/AbouttUs" action>About Us</Link>
             <Link className="list-group-item list-group-item-action " tag="a"  to="/ContactUs" action>Contact Us</Link>
         </ListGroup>
     )
